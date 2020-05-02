8480accc02d134276ab9bc62e0618a0a29b9ba4a
Yifeng Zhu <yifengz2@andrew.cmu.edu>
2020-05-01 20:31:24 -0500

    Update on custom plugin

---
+++ custom-file.el
@@ -0,0 +1,24 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(init--custom-file "~/.emacs.d/custom-file.el")
+ '(org-agenda-files
+   (quote
+    ("~/workspace/Orgfiles/wiki/Literature-Review.org" "/home/yifeng/workspace/Orgfiles/refile.org" "/home/yifeng/workspace/Orgfiles/todo.org")))
+ '(package-selected-packages
+   (quote
+    (gscholar-bibtex ox-reveal epresent helm-bibtexkey interleave rg calfw mu4e-alert deft calfw-cal calfw-ical calfw-org org-ref company-auctex company-rtags clang-format org-super-agenda yasnippet-snippets ranger aggressive-indent auto-yasnippet lua-mode undo-tree protobuf-mode hydra cargo rust-mode helm-projectile rainbow-delimiters transpose-frame cmake-ide irony-eldoc rtags cmake-mode flycheck-mypy elpy pyenv-mode json-mode markdown-mode yaml-mode auctex-latexmk nyan-mode monky magit smartparens helm-swoop helm company yasnippet flycheck ace-window direnv emojify pallet)))
+ '(rhol-default-font "DejaVu Sans Mono")
+ '(rhol-default-font-size 9)
+ '(rhol-gpg-default-key "yifengzhu1129@gmail.com")
+ '(rhol-org-agenda-files (quote ("/home/yifeng/workspace/Orgfiles/")))
+ '(rhol-org-default-notes-file "/home/yifeng/workspace/Orgfiles/refile.org")
+ '(rhol-org-directory "/home/yifeng/workspace/Orgfiles"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
